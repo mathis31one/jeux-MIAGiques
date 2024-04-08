d9eb93eae3b14a8fa08e77d83f740e3f64db8ad4
mathis31one <48861537+mathis31one@users.noreply.github.com>
2024-04-08 11:29:53 +0200

    init

---
+++ src/main/java/com/miage/jeux_miagiques/JeuxMiaGiquesApplication.java
@@ -0,0 +1,13 @@
+package com.miage.jeux_miagiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxMiaGiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuxMiaGiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miage/jeux_miagiques/JeuxMiaGiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.jeux_miagiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuxMiaGiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
