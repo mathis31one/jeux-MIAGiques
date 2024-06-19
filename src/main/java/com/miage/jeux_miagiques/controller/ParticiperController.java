79805d127135b70579beccdee3dc6540aa6ceb07
mathis31one <mathis.oneda31@gmail.com>
2024-06-19 13:41:44 +0200

    création du controller de participer

--- src/main/java/com/miage/jeux_miagiques/controller/ParticiperController.java
+++ src/main/java/com/miage/jeux_miagiques/controller/ParticiperController.java
@@ -1,5 +1,10 @@
 package com.miage.jeux_miagiques.controller;
 
+import com.miage.jeux_miagiques.dao.model.Epreuve;
+import com.miage.jeux_miagiques.dao.model.Participer;
+import com.miage.jeux_miagiques.service.ParticiperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -7,5 +12,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/participer")
 public class ParticiperController {
+    @Autowired
+    private ParticiperService participerService;
 
+
+    // Inscription d'une nouvelle participation
+    @PostMapping("/creation")
+    public ResponseEntity<Participer> creationParticipation(@RequestBody Participer participer) {
+        Participer nouvelleParticipation = participerService.creerParticipation(participer);
+        return ResponseEntity.ok(nouvelleParticipation);
+    }
+
+    // Suppression d'une participation
+    @DeleteMapping("/suppression/{epreuveId}")
+    public ResponseEntity<?> supprimerSpectateur(@PathVariable Long partciperId) {
+        participerService.supprimerParticipation(partciperId);
+        return ResponseEntity.ok("participation supprimée avec succès");
+    }
+
+    @GetMapping("/epreuves")
+    public ResponseEntity<List<Participer>> recupererTousLesSpectateurs() {
+        List<Participer> participations = participerService.recupererToutesLesParticipations();
+        return ResponseEntity.ok(participations);
+    }
 }
